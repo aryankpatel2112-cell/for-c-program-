8350ade61b65e19131395cf5bef440e3cdf9ef8d
aryankpatel2112-cell <aryankpatel2112@gmail.com>
2025-08-30 11:04:10 +0530

    Add files via upload

---
+++ convert rupees into doller.where doller=48rupeeys.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int doller,rupeey
+    printf("enter money in rupeey");
+    scanf("%d",rupeey);
+    doller = rupeey/48;
+    printf("%d = %d / 48",doller,rupeey);
+}
