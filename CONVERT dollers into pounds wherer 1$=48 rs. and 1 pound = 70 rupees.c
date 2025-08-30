30d54b68b71be81f355c8e70646ebd132777f579
aryankpatel2112-cell <aryankpatel2112@gmail.com>
2025-08-30 11:58:55 +0530

    Add files via upload

---
+++ CONVERT dollers into pounds wherer 1$=48 rs. and 1 pound = 70 rupees.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int doller,pound,rupeey;
+    printf("enter money in doller");
+    scanf("%d",doller);
+    rupeey = dolller * 48;
+    pound = rupeey/70
+    printf("%d = %d * 48 = %d / 70",ruppy,doller,pound);
+}
